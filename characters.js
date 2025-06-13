32397846d30be9e2d39c011baec3937e41d6dd9a
Natalie-Dor <varnata000@gmail.com>
2025-06-13 13:49:15 +0300

    added js file

---
+++ characters.js
@@ -0,0 +1,55 @@
+const characters = [
+    {
+    name: "Ellie",
+    age: 19,
+    isImmune: true,
+    location: { city: "Jackson", zone: "+-Safe" },
+    aging() {
+        this.age += 1;
+    }
+    },
+    {
+    name: "Joel",
+    age: 50,
+    isImmune: false,
+    location: { city: "Boston", zone: "Quarantine" },
+    aging() {
+        this.age += 1;
+    }
+    }
+];
+characters.shift();
+characters.push({
+    name: "Dina",
+    age: 20,
+    isImmune: false,
+    location: { city: "Jackson", zone: "+-Safe" },
+    aging() {
+        this.age += 1;
+    }
+})
+characters.forEach((char, index) => char.id = index);
+const filtered = characters.filter(char => char.isImmune);
+filtered.forEach(char => {
+    console.log(`${char.id}. ${char.name}`);
+});
+const sum = characters.reduce((acc, char) => acc + char.age, 0);
+console.log("Total age:", sum);
+const someFunc = () => {
+    const fetchData = async () => {
+        console.log("start")
+        try {
+            const result = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+            const data = await result.json()
+            console.log(data)
+        } catch(e) {
+            console.log("error:", e)
+        }
+
+        console.log("finish")
+    }
+
+    fetchData()
+}
+
+someFunc()
